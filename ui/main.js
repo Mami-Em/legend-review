c0851cbb0d0951144e3bfd0cbc511d9304a911a6
mami-em <maminiainaravaloarison@gmail.com>
2022-12-30 04:18:53 -0500

    search bar slide success

--- ui/main.js
+++ ui/main.js
@@ -24,39 +24,58 @@
 */
 
 
-// menu btn
-const menu = document.querySelector("#menu");
-const menuSection=document.querySelector("#menu-section");
-document.querySelector("#menu-btn").addEventListener("click", function() {
+document.addEventListener("DOMContentLoaded", () => {
 
-  // show menu section
-  menuSection.classList.remove("hidden");
+  // menu btn
+  const menu = document.querySelector("#menu");
+  const menuSection=document.querySelector("#menu-section");
   
-  // rm hide menu anim
-  if (menu.classList.contains("hide-menu")) {
+  // show menu
+  document.querySelector("#menu-btn").addEventListener("click", function() {
+    menuSection.classList.remove("hidden");
     menu.classList.remove("hide-menu");
-  };
+    setTimeout(() => {
+      menu.classList.add("show-menu");
+      menu.classList.remove("left-[-300px]");
+    }, 50);
+  });
+  
+  // hide menu
+  document.querySelector("#hide-menu-btn").addEventListener("click", function() {
+    menu.classList.remove("show-menu");
+    menu.classList.add("hide-menu");
+    menu.classList.add("left-[-300px]");
+    setTimeout(() => {
+      menuSection.classList.add("hidden");
+    },200);
+  
+  });
+  
+  
+  const searchBar = document.querySelector("#search-bar");
+  const searchSection = document.querySelector("#search-section");
+  
+  // show search bar
+  document.querySelector("#search-btn").addEventListener("click", function() {
+    searchSection.classList.remove("hidden");
+    searchBar.classList.remove("slide-sb");
+    setTimeout(() => {
+      searchBar.classList.remove("top-[-60px]");
+      searchBar.classList.add("show-sb");
+    }, 50);
 
-  // slide menu in after .05s
-  setTimeout(() => {
-    menu.classList.add("show-menu");
-    menu.classList.remove("left-[-300px]");
-  }, 50);
-});
-
-// hide menu btn
-document.querySelector("#hide-menu-btn").addEventListener("click", function() {
-
-  // rm show menu anim
-  menu.classList.remove("show-menu");
-
-  // slide out menu
-  menu.classList.add("hide-menu");
-  menu.classList.add("left-[-300px]");
-
-  // rm blur bg after .2s
-  setTimeout(() => {
-    menuSection.classList.add("hidden");
-  },200);
+    document.querySelector("#search").focus();
+  });
+  
+  
+  // close search bar
+  document.querySelector("#close-search").addEventListener("click", function() {
+    searchBar.classList.remove("show-sb"); 
+    searchBar.classList.add("slide-sb");
+    searchBar.classList.add("top-[-60px]");
+    setTimeout(() => {
+      searchSection.classList.add("hidden");
+    }, 200);
+  });
 
 });
