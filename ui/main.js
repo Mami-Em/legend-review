e377d4e972cb873f54a38518fe22adcefcf35ad9
mami-em <maminiainaravaloarison@gmail.com>
2023-01-02 00:40:16 -0500

    ui for details ready

--- ui/main.js
+++ ui/main.js
@@ -82,3 +82,47 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
 });
+
+
+// review stars
+
+const stars = document.querySelectorAll(".review-stars");
+let rated = 0;
+
+stars.forEach(element => {
+  element.addEventListener("click", () => {
+    const selectedRate = element.dataset.rate;
+    rated = selectedRate;
+      stars.forEach(e => {
+        const rate = e.dataset.rate;
+        rate <= selectedRate ? e.classList.add('fill-zinc-900') : e.classList.remove('fill-zinc-900');
+      });
+  });
+});
+
+
+// show review
+const reviewSection = document.querySelector("#review-section");
+const reviewSh = document.querySelector("#review-sh");
+
+
+document.querySelector("#review-btn").addEventListener("click", () => {
+  reviewSection.classList.remove("hidden");
+  reviewSh.classList.remove("hide-review-section");
+  setTimeout(() => {
+    reviewSh.classList.add("show-review-section");
+    reviewSh.classList.remove("-bottom-[50rem]");
+  }, 50);
+});
+
+
+document.querySelector("#close-review-section").addEventListener("click", closeReviewSection);
+
+function closeReviewSection() {
+  reviewSh.classList.remove("show-review-section");
+  reviewSh.classList.add("hide-review-section");
+  reviewSh.classList.add("-bottom-[50rem]");
+  setTimeout(() => {
+    reviewSection.classList.add("hidden");
+  }, 200);
+};
